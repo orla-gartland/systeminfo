abd8063cd84938d09507182f73eb1238bc874b49
orla <orla@orla-SATELLITE-C50D-A-11Q>
2018-01-30 14:36:00 +0000

    inital commit

---
+++ systeminfo/main.py
@@ -0,0 +1,13 @@
+'''
+Created on 30 Jan 2018
+
+@author: orla
+'''
+import platform
+
+def main():
+    print(platform.platform())
+    return 
+
+if __name__ == '__main__':
+    pass
---
+++ systeminfo/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name= "systeminfo",
+      version="0.1",
+      description= "Basic system information for comp30670", 
+      url="",
+      author="Orla Gartland", 
+      author_email="orla.gartland@ucdconnect.ie",
+      licence="GPL3", 
+      packages= ['systeminfo'],
+      entry_points ={
+          'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
+          }
+    )
